e34b43722666038f4c1e06966ad0b8d9f13e96af
arushidua <aarushidua1@gmail.com>
2025-07-11 14:11:19 +0530

    wip

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -2,6 +2,7 @@ import { useRef } from "react";
 import { createContext } from "react";
 import { songsData } from "../assets/assets";
 import { useState } from "react";
+import { useEffect } from "react";
 
 export const PlayerContext = createContext();
 const PlayerContextProvider = (props) => {
@@ -12,7 +13,7 @@ const PlayerContextProvider = (props) => {
 
     const [track, setTrack] = useState(songsData[0]);
     const [playStatus, setPlayStatus] = useState(false);
-    const [time, setTime] = useState([
+    const [time, setTime] = useState(
         {
             currentTime:{
                 second:0,
@@ -22,8 +23,7 @@ const PlayerContextProvider = (props) => {
                 second:0,
                 minute:0,
             }
-        }
-    ])
+        })
 
     const play = () => {
         audioRef.current.play();
@@ -35,6 +35,30 @@ const PlayerContextProvider = (props) => {
          setPlayStatus(false);
     }
 
+    const playWithId = async (id) => {
+        await setTrack(songsData[id]);
+        await audioRef.current.play();
+        setPlayStatus(true)
+    } 
+
+    useEffect(()=>{
+        setTimeout(() => {
+
+            audioRef.current.ontimeupdate = () => {
+                seekBar.current.style.width = (Math.floor(audioRef.current.currentTime/audioRef.current.duration*100))+"%";
+                setTime({currentTime:{
+                second: Math.floor(audioRef.current.currentTime % 60),
+                minute:Math.floor(audioRef.current.currentTime / 60),
+            },
+            totalTime:{
+                second: Math.floor(audioRef.current.duration % 60),
+                minute:Math.floor(audioRef.current.duration  / 60),
+            }})
+            }
+            
+        }, 1000);
+    },[audioRef])
+
     const contextValue = {
         audioRef,
         seekBar,
@@ -45,7 +69,8 @@ const PlayerContextProvider = (props) => {
         setPlayStatus,
         time,
         setTime,
-        play, pause
+        play, pause,
+        playWithId
     }
 
     return (
